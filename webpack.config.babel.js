2afb81bffd339404af7159e61ba884be80df6bca
Subtirelu Mihail <dazzzz2007@gmail.com>
2016-03-16 14:53:28 +0200

    Add build folder and dujmmy index.html

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -43,7 +43,7 @@ module.exports = {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: "./index.html",
+        template: "./src/index.html",
         inject: 'body'
       }),
       new ExtractTextPlugin("[contenthash].css"),
